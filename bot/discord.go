4cac543eea3d8af8264be6a77b64addcaf37a403
basisbit <hp.stefan@gmail.com>
2017-06-13 22:43:55 +0200

    adapt discord.go to work properly after bwmarrin/discordgo@86a21ea

--- bot/discord.go
+++ bot/discord.go
@@ -39,7 +39,7 @@ func dInit() {
 
 	dBotID = u.ID
 
-	guilds, err := dSession.UserGuilds()
+	guilds, err := dSession.UserGuilds(99, "", "")
 	if err != nil {
 		log.Fatalf("Failed to get guilds: %s", err)
 	}
